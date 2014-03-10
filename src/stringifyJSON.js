2d5ea66802a0bf221078fa971e8b03701043f8ae
Andrew Jones <acjones617@gmail.com>
2014-03-10 15:27:46 -0700

    Stringify arrays, objects, strings, and other

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,27 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var finalString = '';
+  if (Array.isArray(obj)) {
+  	finalString += '[';
+  	_.each(obj, function (element, index, list) {
+    	finalString += stringifyJSON(element) + (index !== list.length - 1) ? ',';
+  	});
+  	finalString += ']';
+  } else if (!(obj.length)) {
+  	finalString += '{';
+  	var objElements = false;
+  	_.each(obj, function (propVal, prop, object) {
+  		objElements = true;
+  		finalString += stringifyJSON(prop) + ':' + stringifyJSON(propVal) + ',';
+  	});
+  	if (objElements) finalString = finalString.slice(0,finalString.length-2);
+  	finalString += '}';
+  } else if (_.isString(obj)) {
+  	finalString += '"' + obj + '"';
+  } else {
+  	finalString += obj.toString();
+  }
+
+  return finalString;
 };
