914ed9ebe7a0972fad31490dc91b857aa5615fba
Andrew Jones <acjones617@gmail.com>
2014-03-10 15:48:27 -0700

    Adding test cases for stringify

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,13 +1,19 @@
 // test cases are described in fixtures.js
 describe("stringifyJSON", function(){
   it("should match the result of calling JSON.stringify", function(){
-
+    
+    stringifiableValues.forEach(function(obj){
+      var result = stringifyJSON(obj);
+      var expected = JSON.stringify(obj);
+      expect(result).toEqual(expected);
+    });
+/*
     arrayWithValidElements.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
       expect(result).toEqual(expected);
     });
-
+*/
     objectWithInvalidAttributes.forEach(function(obj){
       var result = stringifyJSON(obj);
       var expected = JSON.stringify(obj);
