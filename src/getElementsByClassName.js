95592bd068bb5386b06439e3ff6f48a9b44ffed0
Andrew Jones <acjones617@gmail.com>
2014-03-11 02:14:13 -0700

    Passes first test. First dirty pass.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,38 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-  // your code here
+// But instead we're going to implement it from scratch:
+var getElementsByClassName = function (className, nodes, nList) {
+  // too easy --> return $('.'+className);
+  
+  // "NodeList" returned by function { 0 : HTMLNode, 1 : HTMLNode, length : 2, 
+  	//prevObject : { 0 : HTMLNode, context : HTMLNode, length : 1 }, context : HTMLNode, selector : '.targetClassName' }.
+ 
+
+  var NodeList = [];
+
+  if (nList) {
+  	NodeList = nList;
+  }
+
+  if (!nodes) {
+  	nodes = document.body;
+  }
+
+  // You should use document.body, element.childNodes, and element.classList
+  _.each(nodes.childNodes, function (element) {	
+  	_.each(element.classList, function (propVal, prop) {
+  		if (propVal == className) {
+  			NodeList.push(element);
+  		}
+  	});
+
+  	if (element.childNodes) {
+  		getElementsByClassName(className, element, NodeList);
+  	}
+  	
+  });
+
+  return NodeList;
+
 };
