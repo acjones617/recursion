b756b91642d17a5500aaccd6291c451284022ca8
Andrew Jones <acjones617@gmail.com>
2014-03-12 15:59:24 -0700

    Try writing code if parse encounters array

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,23 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  var jText = json;
+  var jProd;
+
+  var makeArray = function (text, finalProd) {
+  	return finalProd + [].push(parseJSON(text));
+  }
+
+  for (var i = 0; i < json.length; i++) {
+  	// if Array:
+  	if (json[i] = '[') {
+  		var endArr = json.indexOf(']', i);
+  		jProd = makeArray(json.slice(i + 1, endArr), jProd);
+  		i = endArr;
+  	}
+
+  	// if Object:
+
+  }
+
 };
