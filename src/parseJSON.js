57b4335c6a5df4ed5a2aa4b81cfe6e299bab1e62
Andrew Jones <acjones617@gmail.com>
2014-03-12 16:08:40 -0700

    Add boolean and string conditions. Still returning undefined.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,18 +7,37 @@ var parseJSON = function (json) {
   var jProd;
 
   var makeArray = function (text, finalProd) {
-  	return finalProd + [].push(parseJSON(text));
+  	return [].push(parseJSON(text));
+  }
+
+  var makeString = function (text, finalProd) {
+  	return text;
   }
 
   for (var i = 0; i < json.length; i++) {
   	// if Array:
-  	if (json[i] = '[') {
+  	if (json[i] === '[') {
   		var endArr = json.indexOf(']', i);
-  		jProd = makeArray(json.slice(i + 1, endArr), jProd);
+  		jProd += makeArray(json.slice(i + 1, endArr));
   		i = endArr;
   	}
 
   	// if Object:
+
+  	// if String:
+  	if (json[i] === '"') {
+  		var endArr = json.indexOf('"', i+1);
+  		jProd += makeString(json.slice(i + 1, endArr), jProd);
+  	}
+
+  	// if Boolean:
+  	if (json.slice(i,i+4) === 'true') {
+  		jProd += true;
+  	}
+
+  	if (json.slice(i,i+5) === 'false') {
+  		jProd += false;
+  	}
 
   }
 
